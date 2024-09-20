2c52aed4e9ea549c916ae4d039ac6dc8a622a736
SEC-MASUM <masum.jess42@gmail.com>
2024-09-20 12:14:04 +0600

    install http package, Create Product Model, implement ProductList Get functionality using api

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_live_class/models/product.dart';
 import 'package:flutter_live_class/screens/add_new_product_screen.dart';
 import 'package:flutter_live_class/widgets/product_item.dart';
+import 'package:http/http.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -10,24 +14,45 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List<Product> productList = [];
+  bool _inProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Product List"),
+        actions: [
+          IconButton(
+            onPressed: getProductList,
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView.separated(
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return const ProductItem();
-          },
-          separatorBuilder: (context, index) {
-            return const SizedBox(height: 16);
-          },
-        ),
-      ),
+      body: _inProgress
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListView.separated(
+                itemCount: productList.length,
+                itemBuilder: (context, index) {
+                  return ProductItem(
+                    product: productList[index],
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 16);
+                },
+              ),
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -37,5 +62,33 @@ class _ProductListScreenState extends State<ProductListScreen> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> getProductList() async {
+    _inProgress = true;
+    setState(() {});
+    Uri uri = Uri.parse("http://164.68.107.70:6060/api/v1/ReadProduct");
+    Response response = await get(uri);
+    print(response);
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      productList.clear();
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      for (var item in jsonResponse["data"]) {
+        Product product = Product(
+            id: item["_id"],
+            productName: item["ProductName"],
+            productCode: item["ProductCode"],
+            img: item["Img"],
+            unitPrice: item["UnitPrice"],
+            qty: item["Qty"],
+            totalPrice: item["TotalPrice"],
+            createdDate: item["CreatedDate"]);
+        productList.add(product);
+      }
+    }
+    _inProgress = false;
+    setState(() {});
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_live_class/models/product.dart';
 import 'package:flutter_live_class/screens/update_product_screen.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
     super.key,
+    required this.product,
   });
+
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +16,16 @@ class ProductItem extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
       ),
-      title: const Text("Product Name"),
+      title: Text(product.productName),
       tileColor: Colors.white,
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text("Product Code : CODE"),
-          const Text("Price : \$120"),
-          const Text("Quantity : 2"),
-          const Text("Total Price : \$240"),
-          const Divider(),
+          Text("Product Code : ${product.productCode}"),
+          Text("Price : \$${product.unitPrice}"),
+          Text("Quantity : ${product.qty}"),
+          Text("Total Price : \$${product.totalPrice}"),
+          Divider(),
           ButtonBar(
             children: [
               TextButton.icon(
